112d216a1b321a712ea0aceab9d2fc60c999a0ae
rafalimma <rafaelglima79@gmail.com>
2025-03-21 21:49:18 -0300

    endpoint que retorna employees + emails por periodo

--- ona/models.py
+++ ona/models.py
@@ -13,13 +13,13 @@ class Employee(models.Model):
     lider = models.CharField(max_length=100, default='')
     depart = models.ForeignKey(Departs, on_delete=models.CASCADE, null=True, blank=True)
     idade = models.CharField(max_length=10, default='')
-    level = models.CharField(max_length=100, default='') # senioridade
+    senioridade = models.CharField(max_length=100, default='') # descritivo
+    level = models.IntegerField() # numerico
     genero = models.CharField(max_length=50, default='')
     raca = models.CharField(max_length=50, default='')
     nacionalidade = models.CharField(max_length=50, default='')
-    cidade = models.CharField(max_length=100, default='')
-    # cargo
-    tipo_colaborador = models.CharField(max_length=50, default='') # tirar 
+    estado = models.CharField(max_length=100, default='')
+    tipo_colaborador = models.CharField(max_length=50, default='') # estagiário, CLT, PJ, consultoria etc
     salario = models.CharField(max_length=50, default='')
     data_nascimento = models.DateField()
     data_onbording = models.DateField()
@@ -29,7 +29,6 @@ class Employee(models.Model):
         return self.nome
 
 class EmailTalks(models.Model):
-    tipo = models.CharField(max_length=50, default='') # tirar
     origem = models.CharField(max_length=30, default='') # calendar_api / gmail_api
     hash_id = models.CharField(max_length=200, default='')
     remetente = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='emails_enviados') # id do colaborador
